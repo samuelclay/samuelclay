b71192dc026e6fe675ad9be7de82668b88b7e1d9
Samuel Clay <samuel@ofbrooklyn.com>
2010-02-04 00:08:52 -0500

    Adding Scheduler Jones and CaseLife pages/images.

--- urls.py
+++ urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^/?$', 'com.views.index'),
+    url(r'^/?$', 'com.views.index', name="index"),
+    url(r'^schedulerjones/?$', direct_to_template, {'template': 'schedulerjones.html'}, name="schedulerjones"),
+    url(r'^caselife/?$', direct_to_template, {'template': 'caselife.html'}, name="caselife"),
     (r'^raphael/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT+'/../raphael'}),
     (r'^schedulerjones/(?P<path>.*)$', 'django.views.static.serve',
