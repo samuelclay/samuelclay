b30d8759748039587b1fe215527b5e0d64acecbb
Samuel Clay <samuel@ofbrooklyn.com>
2020-02-03 18:45:12 -0500

    Includes, not contains

--- wedding.js
+++ wedding.js
@@ -1,9 +1,9 @@
 var http = require("http");
 
 http.createServer(function (req, res) {
-    if (req.url.contains('cleveland')) {
+    if (req.url.includes('cleveland')) {
         res.writeHead(301, {"Location": "https://docs.google.com/forms/d/e/1FAIpQLSeu9P5tKF0P4L8pBYb7q7U8AZbnUEcRE9-AxguHhqYOr4aKGA/viewform"});
-    } else if (req.url.contains('pismo')) {
+    } else if (req.url.includes('pismo')) {
         res.writeHead(301, {"Location": "https://docs.google.com/forms/d/e/1FAIpQLSebCW_2RHvcyNVxR8vi0FNqsnzfng6_uHGD3f8OgjUNun7oSg/viewform"});
     } else {
         res.writeHead(301, {"Location": "https://withjoy.com/10ten20twenty"});
